18ffbab8f2079cdf054baf24e22eba16f03f68e5
oran rozenbaum <oran.rozenbaum@gmail.com>
2019-04-11 16:36:47 +0300

    update

--- correlation.py
+++ correlation.py
@@ -1,7 +1,17 @@
 from difflib import SequenceMatcher
+import spacy
+
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 
-similar("Apple","Appel")
+print(similar("horse", "ors"))
+
+
+nlp = spacy.load('en_core_web_lg')  # make sure to use larger model!
+tokens = nlp(u'lion tiger')
+
+for token1 in tokens:
+    for token2 in tokens:
+        print(token1.text, token2.text, token1.similarity(token2))
