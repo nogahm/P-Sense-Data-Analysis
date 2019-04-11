c2bc8e59028f1a11f5484e84da1bbd0f1f7ca73b
oran rozenbaum <oran.rozenbaum@gmail.com>
2019-04-11 17:09:22 +0300

    correlation

--- correlation.py
+++ correlation.py
@@ -6,12 +6,17 @@ def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 
-print(similar("horse", "ors"))
+def sentiment_analysis(a, b):
+    nlp = spacy.load('en_core_web_lg')  # make sure to use larger model!
+    string = a+' '+b
+    tokens = nlp(string)
+    return tokens[0].similarity(tokens[1])
 
 
-nlp = spacy.load('en_core_web_lg')  # make sure to use larger model!
-tokens = nlp(u'lion tiger')
+def calculate_correlation(a, b):
+    sim = similar(a, b)
+    if sim > 0.6:
+        return 1
+    else:
+        return sentiment_analysis(a, b)
 
-for token1 in tokens:
-    for token2 in tokens:
-        print(token1.text, token2.text, token1.similarity(token2))
