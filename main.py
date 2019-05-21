021ed184b4b81a80e5f7475f70980e2c14eb1322
nogahm <nogahm@post.bgu.ac.il>
2019-05-21 14:38:44 +0300

    20.05.19 - new tables

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import Graphs
 def main(dataPath):
     PreProcess.csvPath=dataPath
     PreProcess.ReadFilesToDataFrame()
-    # PreProcess.SplitToPictureAndFacesAnswers()
-    # BadParticipantsRemove.RemoveParticipantsNotReportPANAS()
-    # PreProcess.GetTestRates()
-    # PreProcess.SaveTests()
-    # PreProcess.LoadTestsWithScores()
-    #
-    # PreProcess.GetTestMovie()
-    # PreProcess.addUserInfoToTest()
-    # PreProcess.SaveTests()
+    PreProcess.SplitToPictureAndFacesAnswers()
+    BadParticipantsRemove.RemoveParticipantsNotReportPANAS()
+    PreProcess.GetTestRates()
+    PreProcess.SaveTests()
+    PreProcess.LoadTestsWithScores()
+
+    PreProcess.GetTestMovie()
+    PreProcess.addUserInfoToTest()
+    PreProcess.SaveTests()
     PreProcess.LoadTestsWithScores()
     # Graphs.ShowFalsePositiveGraphs(PreProcess.TestsWithScores.loc[PreProcess.TestsWithScores['video']=='calm'])
     # Graphs.ShowDifferenceBetweenTests(PreProcess.TestsWithScores)
